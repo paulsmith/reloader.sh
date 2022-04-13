046ed618b4a279e0773b0c7e935c21727daa7733
Paul Smith <paulsmith@gmail.com>
2022-04-12 23:01:09 -0500

    Install program via flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,26 @@
 {
-    description = "Experiments and research for a web forms language";
+    description = "reload program when file changes detected";
 
-    inputs.nixpkgs.url = "nixpkgs/nixos-unstable";
     inputs.flake-utils.url = "github:numtide/flake-utils";
 
     outputs = { self, nixpkgs, flake-utils }:
-        flake-utils.lib.eachDefaultSystem
-            (system:
-                let pkgs = nixpkgs.legacyPackages.${system}; in
-                {
-                  devShell = pkgs.mkShell {
-                    buildInputs = with pkgs; [ fswatch ];
-                  };
-                }
-            );
+      flake-utils.lib.eachDefaultSystem (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          my-name = "reloader";
+          my-build-inputs = with pkgs; [ fswatch ];
+          my-src = builtins.readFile ./reloader.sh;
+          my-script = (pkgs.writeShellScriptBin my-name my-src).overrideAttrs(old: {
+            buildCommand = "${old.buildCommand}\n patchShebangs $out";
+          });
+        in rec {
+          defaultPackage = packages.my-script;
+          packages.my-script = pkgs.symlinkJoin {
+            name = my-name;
+            paths = [ my-script ] ++ my-build-inputs;
+            buildInputs = [ pkgs.makeWrapper ];
+            postBuild = "wrapProgram $out/bin/${my-name} --prefix PATH : $out/bin";
+          };
+        }
+    );
 }
